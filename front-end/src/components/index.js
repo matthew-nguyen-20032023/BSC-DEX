959d1aa3c2e000667c3a442a9cc9b4835848c8a2
matthew.nguyen <matthew.nguyen.20.03.2023@gmail.com>
2023-07-11 16:44:06 +0700

    remove un use FE code

--- front-end/src/components/index.js
+++ front-end/src/components/index.js
@@ -1,15 +1,5 @@
-import BaseCheckbox from "./BaseCheckbox.vue";
-import BaseDropdown from "./BaseDropdown.vue";
-import BaseButton from "./BaseButton";
 import BaseAlert from "./BaseAlert";
-import BaseNav from "./BaseNav";
-import Modal from "./Modal";
 
 export {
-  Modal,
-  BaseCheckbox,
-  BaseDropdown,
-  BaseButton,
   BaseAlert,
-  BaseNav
 };
--- front-end/src/plugins/globalComponents.js
+++ front-end/src/plugins/globalComponents.js
@@ -1,13 +1,9 @@
-import { BaseDropdown, BaseButton, BaseCheckbox } from "../components/index";
 /**
  * You can register global components here and use them as a plugin in your main Vue instance
  */
 
 const GlobalComponents = {
   install(Vue) {
-    Vue.component(BaseDropdown.name, BaseDropdown);
-    Vue.component(BaseButton.name, BaseButton);
-    Vue.component(BaseCheckbox.name, BaseCheckbox);
   }
 };
 
