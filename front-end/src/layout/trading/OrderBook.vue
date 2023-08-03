223058ff18cde58d2c9cdbd04a35b0b5b13d2ae7
matthew.nguyen <matthew.nguyen.20.03.2023@gmail.com>
2023-08-04 02:32:11 +0700

    feature: improve UI and performance

--- front-end/src/layout/trading/OrderBook.vue
+++ front-end/src/layout/trading/OrderBook.vue
@@ -1,6 +1,6 @@
 <template>
   <b-row class="ml-1 mt-2">
-    <table style="font-size: 12px; color: #e54150">
+    <table id="sellOrders" style="font-size: 12px; color: #e54150">
       <tr>
         <th style="text-align: center; color: #525f7f" colspan="3"><strong>Order Book</strong></th>
       </tr>
@@ -9,24 +9,28 @@
         <th>Amount</th>
         <th>Total</th>
       </tr>
-      <tr v-for="(order, index) in sellOrders" :key="index">
-        <td>{{ order.price }}</td>
-        <td>{{ order.amount }}</td>
-        <td>{{ order.total }}</td>
-      </tr>
+      <tbody>
+      </tbody>
+<!--      <tr v-for="(order, index) in sellOrders" :key="index">-->
+<!--        <td>{{ order.price }}</td>-->
+<!--        <td>{{ order.amount }}</td>-->
+<!--        <td>{{ order.total }}</td>-->
+<!--      </tr>-->
     </table>
     <hr>
-    <table style="font-size: 12px; color: #23a776">
+    <table id="buyOrders" style="font-size: 12px; color: #23a776">
       <tr style="color: rgb(132, 142, 156)">
         <th>Price ({{quoteTokenSymbol}})</th>
         <th>Amount</th>
         <th>Total</th>
       </tr>
-      <tr v-for="(order, index) in buyOrders" :key="index">
-        <td>{{ order.price }}</td>
-        <td>{{ order.amount }}</td>
-        <td>{{ order.total }}</td>
-      </tr>
+      <tbody>
+      </tbody>
+<!--      <tr v-for="(order, index) in buyOrders" :key="index">-->
+<!--        <td>{{ order.price }}</td>-->
+<!--        <td>{{ order.amount }}</td>-->
+<!--        <td>{{ order.total }}</td>-->
+<!--      </tr>-->
     </table>
   </b-row>
 </template>
@@ -57,16 +61,6 @@ export default {
       this.getOrderBook();
       if (newVal) this.initSocketOrderBook()
     },
-    buyOrders() {
-      if (this.buyOrders.length > this.defaultLengthOrderBook) {
-        this.buyOrders = this.buyOrders.splice(0, this.defaultLengthOrderBook);
-      }
-    },
-    sellOrders() {
-      if (this.sellOrders.length > this.defaultLengthOrderBook) {
-        this.sellOrders = this.sellOrders.splice(0, this.defaultLengthOrderBook);
-      }
-    }
   },
   created: debounce(function () {
     this.getOrderBook();
@@ -83,6 +77,13 @@ export default {
       socket.on(`OrderBookByPair_${this.pairId}`, (orderBook) => {
         this.buyOrders = orderBook.buyOrders;
         this.sellOrders = orderBook.sellOrders;
+        if (this.sellOrders.length > this.defaultLengthOrderBook) {
+          this.sellOrders = this.sellOrders.splice(0, this.defaultLengthOrderBook);
+        }
+        if (this.buyOrders.length > this.defaultLengthOrderBook) {
+          this.buyOrders = this.buyOrders.splice(0, this.defaultLengthOrderBook);
+        }
+        this.updateOrderBook();
       })
     },
     getOrderBook() {
@@ -110,6 +111,45 @@ export default {
     calculateTotal(order) {
       // Calculate the total value for each order
       return order.price * order.size;
+    },
+    updateOrderBook() {
+      const buyOrdersTable = document.getElementById("buyOrders").querySelector("tbody");
+      const sellOrdersTable = document.getElementById("sellOrders").querySelector("tbody");
+
+      // Clear existing rows
+      buyOrdersTable.innerHTML = "";
+      sellOrdersTable.innerHTML = "";
+
+      // Find the highest total
+      const highestTotalBuy = Math.max(...this.buyOrders.map((order) => order.total));
+      const highestTotalSell = Math.max(...this.sellOrders.map((order) => order.total));
+      const highestTotal = Math.max(highestTotalBuy, highestTotalSell);
+
+      // Add buy orders dynamically
+      this.buyOrders.forEach((order) => {
+        const row = buyOrdersTable.insertRow();
+        const priceCell = row.insertCell(0);
+        const amountCell = row.insertCell(1);
+        const totalCell = row.insertCell(2);
+        priceCell.innerText = order.price;
+        amountCell.innerText = order.amount;
+        totalCell.innerText = order.total;
+        const heightPercentage = (order.total / highestTotal) * 100;
+        row.style.backgroundImage = `linear-gradient(to right, rgba(35, 167, 118, 0.1) ${heightPercentage}%, transparent ${heightPercentage}%)`;
+      });
+
+      // Add sell orders dynamically
+      this.sellOrders.forEach((order) => {
+        const row = sellOrdersTable.insertRow();
+        const priceCell = row.insertCell(0);
+        const amountCell = row.insertCell(1);
+        const totalCell = row.insertCell(2);
+        priceCell.innerText = order.price;
+        amountCell.innerText = order.amount;
+        totalCell.innerText = order.total;
+        const heightPercentage = (order.total / highestTotal) * 100;
+        row.style.backgroundImage = `linear-gradient(to right, rgba(229, 65, 80, 0.1) ${heightPercentage}%, transparent ${heightPercentage}%)`;
+      });
     }
   }
 };
